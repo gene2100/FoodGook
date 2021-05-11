6fb74646472a6e6e69cd3d9516ca2ee98df97386
gene2100 <genelnw.mc@gmail.com>
2021-05-12 03:08:31 +0700

    Merge branch 'Gene-Local-Test-Branch-DO-NOT-REMOVE-PLZ-V2' into main

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,6 +1,67 @@
+import 'package:country_pickers/country.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
+
+import 'package:intl/intl.dart';
+import 'package:country_pickers/country_pickers.dart';
 // import 'package:email_validator/email_validator.dart';
+
+final TextEditingController _emailController = TextEditingController();
+final TextEditingController _usernameController = TextEditingController();
+final TextEditingController _passwordController = TextEditingController();
+String _gender = "Male";
+DateTime selectedDOB = DateTime.now();
+String selectedCountry = "United States";
+
+FirebaseAuth _auth = FirebaseAuth.instance;
+FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+enum _registerStatus {success, emailAlreadyUse}
+
+_registerStatus registerStatus = _registerStatus.success;
+
+void register() async{
+  try {
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text
+    );
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+      registerStatus = _registerStatus.emailAlreadyUse;
+    }
+  } catch (e) {
+    print(e);
+  }
+  if(_auth.currentUser != null)
+    {
+      registerStatus = _registerStatus.success;
+      CollectionReference users = FirebaseFirestore.instance.collection('User_Profile');
+      await users.doc(_auth.currentUser.uid).set({
+          'Email': _emailController.text,
+          'Username': _usernameController.text,
+          'Gender': _gender,
+          'DOB': selectedDOB,
+          'Nationality': selectedCountry,
+          'Description': '',
+          'Followers' : 0,
+          'Following' : 0,
+          'Recipes' : 0
+      }).then((value) => print("User Added"))
+      .catchError((error) => print("Failed to add user: $error"));
+      print(_emailController.text);
+      print(_passwordController.text);
+      print(_usernameController.text);
+      print(_gender);
+      print(selectedDOB);
+      print(selectedCountry);
+      print("UID:" + _auth.currentUser.uid);
+      await _auth.signOut();
+    }
+}
 
 class SignupPage extends StatefulWidget {
   @override
@@ -8,11 +69,86 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+
+  final validEmailCharacters = RegExp(r'^[a-zA-Z0-9@\.]+$');
+  final validCharacters = RegExp(r'^[a-zA-Z0-9&%=]+$');
+
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+              Text(((){
+                if(registerStatus == _registerStatus.success){
+                  return "Register Succesful";
+                }else if(registerStatus == _registerStatus.emailAlreadyUse)
+                return "Email already in use";
+              })())
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                if(registerStatus == _registerStatus.success){
+                  _gender = 'Male';
+                  selectedDOB = DateTime.now();
+                  _emailController.clear();
+                  _usernameController.clear();
+                  _passwordController.clear();
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                }else if(registerStatus == _registerStatus.emailAlreadyUse)
+                  Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDOB,
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedDOB)
+      setState(() {
+        selectedDOB = picked;
+      });
+  }
+
+ Widget  _buildDropdownItem(Country country) => Container(
+    child: Row(
+        children: <Widget>[
+        CountryPickerUtils.getDefaultFlagImage(country),
+        SizedBox(
+          width: 10.0,
+        ),
+        Container(width: 250,
+        child: Text("${country.name}", overflow: TextOverflow.ellipsis),
+        )],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+        body: SingleChildScrollView(
+          child: Form(
+          key: _formKey,
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
             Widget>[
           Container(
             child: Stack(
@@ -42,7 +178,8 @@ class _SignupPageState extends State<SignupPage> {
               padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
               child: Column(
                 children: <Widget>[
-                  TextField(
+                  TextFormField(
+                    controller: _emailController,
                     decoration: InputDecoration(
                         labelText: 'EMAIL',
                         labelStyle: TextStyle(
@@ -54,9 +191,17 @@ class _SignupPageState extends State<SignupPage> {
                         // hintStyle: ,
                         focusedBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Color(0xffff6240)))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty || !validEmailCharacters.hasMatch(value) || !value.contains('@') ){
+                        return 'Please enter valid email';
+                      }
+                      else
+                        return null;
+                    },
                   ),
                   SizedBox(height: 10.0),
-                  TextField(
+                  TextFormField(
+                    controller: _usernameController,
                     decoration: InputDecoration(
                         labelText: 'USERNAME',
                         labelStyle: TextStyle(
@@ -65,35 +210,136 @@ class _SignupPageState extends State<SignupPage> {
                             color: Colors.grey),
                         focusedBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Color(0xffff6240)))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty || !validCharacters.hasMatch(value) || value.length < 8 || value.length > 16){
+                        return 'Username must contain 8 - 16 letters or numbers';
+                      }
+                      else
+                        return null;
+                    },
                   ),
                   SizedBox(height: 10.0),
-                  TextField(
+                  TextFormField(
+                    controller: _passwordController,
                     decoration: InputDecoration(
                         labelText: 'PASSWORD ',
                         labelStyle: TextStyle(
                             fontFamily: 'Rublik',
                             fontWeight: FontWeight.bold,
                             color: Colors.grey),
-                        hintText: "Must have at least 6 characters",
+                        hintText: "Must have at least 8 characters",
                         focusedBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Color(0xffff6240)))),
                     obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty || !validCharacters.hasMatch(value) || value.length < 8 || value.length > 16){
+                        return 'Password must contain 8 - 16 letters or numbers';
+                      }
+                      else
+                        return null;
+                    },
                   ),
                   SizedBox(height: 10.0),
-                  TextField(
+                  TextFormField(
                     decoration: InputDecoration(
-                        labelText: 'PHONE',
+                        labelText: 'CONFIRM PASSWORD ',
                         labelStyle: TextStyle(
-                            fontFamily: 'rublik',
+                            fontFamily: 'Rublik',
                             fontWeight: FontWeight.bold,
                             color: Colors.grey),
+                        hintText: "Must have at least 8 characters",
                         focusedBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Color(0xffff6240)))),
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    // Only numbers can be entered
+                    obscureText: true,
+                    validator: (value) {
+                      if (value != _passwordController.text ){
+                        return 'Password must match';
+                      }
+                      else
+                        return null;
+                    },
+                  ),
+                  SizedBox(height: 30.0),
+                    Container(
+                        alignment: Alignment.bottomLeft,
+                    child: Text('GENDER',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                      fontFamily: 'rublik',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.grey),)),
+                  SizedBox(height: 5.0),
+                  Container(
+                      alignment: Alignment.bottomLeft,
+                  child: DropdownButton<String>(
+                    value: _gender,
+                    icon: Icon(Icons.arrow_downward),
+                      iconSize: 20,
+                      elevation: 16,
+                      style: TextStyle(color : Color(0xffff6240),
+                        fontFamily: 'rublik',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                      underline: Container(
+                        height: 2,
+                        color: Color(0xffff6240)
+                      ),
+                      onChanged: (String newValue){
+                        setState(() {
+                          _gender = newValue;
+                      });
+                      },
+                      items: <String>["Male", "Female"].map<DropdownMenuItem<String>>
+                      ((String value) {
+                        return DropdownMenuItem<String>(
+                        value: value,
+                        child : Text(value),
+                        );
+                      }
+                    ).toList(),
+                  )),
+                  SizedBox(height: 15.0),
+                  Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Text('DATE OF BIRTH',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                            fontFamily: 'rublik',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.grey),)),
+                  SizedBox(height: 10.0),
+                  Container(
+                      alignment: Alignment.bottomLeft,
+                    child: GestureDetector(
+                      onTap: () => _selectDate(context),
+                      child : Text(
+                      "${DateFormat.yMd().format(selectedDOB.toLocal())}",
+                         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xffff6240),
+                             decoration: TextDecoration.underline,decorationThickness: 2
+                         ),
+                       ),
+                    )),
+                  SizedBox(height: 15.0),
+                  Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Text('NATIONALITY',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                            fontFamily: 'rublik',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.grey),)),
+                    SizedBox(height: 5.0),
+                  CountryPickerDropdown(
+                    initialValue: 'US',
+                    itemBuilder: _buildDropdownItem,
+                    onValuePicked: (Country country) {
+                      setState(() {
+                        selectedCountry = country.name;
+                      });
+                    },
                   ),
                   SizedBox(height: 50.0),
                   Container(
@@ -103,8 +349,18 @@ class _SignupPageState extends State<SignupPage> {
                         shadowColor: Colors.orangeAccent,
                         color: Color(0xffff6240),
                         elevation: 7.0,
-                        child: GestureDetector(
-                          onTap: () {},
+                        child: InkWell(
+                          onTap: () async {
+                            if (_formKey.currentState.validate()) {
+                              await register();
+                              print('Validate');
+                              _showMyDialog();
+                            }
+                            else
+                              {
+                                print('Error');
+                              }
+                          },
                           child: Center(
                             child: Text(
                               'SIGN UP',
@@ -130,6 +386,11 @@ class _SignupPageState extends State<SignupPage> {
                           borderRadius: BorderRadius.circular(20.0)),
                       child: InkWell(
                         onTap: () {
+                          _gender = 'Male';
+                          selectedDOB = DateTime.now();
+                          _emailController.clear();
+                          _usernameController.clear();
+                          _passwordController.clear();
                           Navigator.of(context).pop();
                         },
                         child: Center(
@@ -141,8 +402,11 @@ class _SignupPageState extends State<SignupPage> {
                       ),
                     ),
                   ),
+                  SizedBox(height: 20.0)
                 ],
               )),
-        ]));
+        ]))));
+
   }
 }
+
