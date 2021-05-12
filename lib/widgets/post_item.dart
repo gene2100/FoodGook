f3a82620e77bc97d6682146339eed3f2bf4d104b
punymook <punymook@gmail.com>
2021-05-12 22:48:41 +0700

    Update post_item.dart

--- lib/widgets/post_item.dart
+++ lib/widgets/post_item.dart
@@ -20,43 +20,116 @@ class PostItem extends StatefulWidget {
 class _PostItemState extends State<PostItem> {
   @override
   Widget build(BuildContext context) {
+    // return Padding(
+    //   padding: EdgeInsets.symmetric(vertical: 5),
+    //   child: InkWell(
+    //     child: Column(
+    //       children: <Widget>[
+    //         ListTile(
+    //           onTap: () => print('Move to profile'),
+    //           leading: CircleAvatar(
+    //             backgroundImage: AssetImage(
+    //               "${widget.dp}",
+    //             ),
+    //           ),
+    //           contentPadding: EdgeInsets.all(0),
+    //           title: Text(
+    //             "${widget.name}",
+    //             style: TextStyle(
+    //               fontWeight: FontWeight.bold,
+    //             ),
+    //           ),
+    //           subtitle: Text(
+    //             //from trailing
+    //             "${widget.time}",
+    //             style: TextStyle(
+    //               fontWeight: FontWeight.w300,
+    //               fontSize: 11,
+    //             ),
+    //           ),
+    //           trailing: TextButton(
+    //             child: Icon(Icons.bookmark_border),
+    //             // onPressed: () => print('save'),
+    //           ),
+
+    //           // onTap: () => print('save'),
+    //         ),
+    //         Image.asset(
+    //           "${widget.img}",
+    //           height: 170,
+    //           width: MediaQuery.of(context).size.width,
+    //           fit: BoxFit.cover,
+    //         ),
+    //       ],
+    //     ),
+    //     onTap: () {},
+    //   ),
+    // );
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 5),
-      child: InkWell(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage(
-                  "${widget.dp}",
+        padding: EdgeInsets.symmetric(vertical: 5),
+        child: Wrap(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage(
+                        "${widget.dp}",
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${widget.name}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "${widget.time}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 11,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              contentPadding: EdgeInsets.all(0),
-              title: Text(
-                "${widget.name}",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                //from trailing
-                "${widget.time}",
-                style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 11,
-                ),
-              ),
+                Icon(Icons.more_horiz, size: 30, color: Colors.grey),
+              ],
             ),
-            Image.asset(
-              "${widget.img}",
-              height: 170,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
+            Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Column(
+                children: [
+                  "${widget.img}" != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            child: Image.asset(
+                              "${widget.img}",
+                              height: 170,
+                              width: MediaQuery.of(context).size.width,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      : SizedBox.shrink(),
+                  SizedBox(height: 8.0),
+                  // Text(nf.content,
+                  //     style: myStyle(
+                  //         color: Colors.grey, fontWeight: FontWeight.w400)),
+                ],
+              ),
             ),
           ],
-        ),
-        onTap: () {},
-      ),
-    );
+        ));
   }
 }
