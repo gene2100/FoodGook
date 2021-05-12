f58f115cf240189418e55f2c84b0aae334b3493b
punymook <punymook@gmail.com>
2021-05-13 02:43:07 +0700

    Add food imgs

--- lib/data.dart
+++ lib/data.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 Random random = Random();
 List names = [
-  "mookmook",
+  "punymook",
   "chatrin20.zzz",
   "phatto_z",
   "joyfoyy",
@@ -15,36 +15,51 @@ List names = [
   "akari___0302",
 ];
 
-List notifs = [
-  "${names[random.nextInt(10)]} and ${random.nextInt(100)} others liked your post",
-  "${names[random.nextInt(10)]} mentioned you in a comment",
-  "${names[random.nextInt(10)]} shared your post",
-  "${names[random.nextInt(10)]} commented on your post",
-  "${names[random.nextInt(10)]} replied to your comment",
-  "${names[random.nextInt(10)]} reacted to your comment",
-  "${names[random.nextInt(10)]} asked you to join a Group️",
-  "${names[random.nextInt(10)]} asked you to like a page",
-  "You have memories with ${names[random.nextInt(10)]}",
-  "${names[random.nextInt(10)]} Tagged you and ${random.nextInt(100)} others in a post",
-  "${names[random.nextInt(10)]} Sent you a friend request",
+List foodnames = [
+  "Homemade Shoyu Ramen",
+  "Air Fryer Cinnamon Rolls",
+  "Tres Leches Cake",
+  "Chicken Biscuit Potpie",
+  "Ham & Spinach Casserole",
+  "Black Bean Burritos",
+  "Shrimp Pad Thai",
+  "Turkey Scallopini",
+  "Garlic Chicken with Herbs",
+  "Veggie Tacos",
+  "Chocolate Pudding",
 ];
 
-List notifications = List.generate(
-    13,
-    (index) => {
-          "name": names[random.nextInt(10)],
-          "dp": "assets/images/cm${random.nextInt(10)}.jpeg",
-          "time": "${random.nextInt(50)} min ago",
-          "notif": notifs[random.nextInt(10)]
-        });
+// List notifs = [
+//   "${names[random.nextInt(10)]} and ${random.nextInt(100)} others liked your post",
+//   "${names[random.nextInt(10)]} mentioned you in a comment",
+//   "${names[random.nextInt(10)]} shared your post",
+//   "${names[random.nextInt(10)]} commented on your post",
+//   "${names[random.nextInt(10)]} replied to your comment",
+//   "${names[random.nextInt(10)]} reacted to your comment",
+//   "${names[random.nextInt(10)]} asked you to join a Group️",
+//   "${names[random.nextInt(10)]} asked you to like a page",
+//   "You have memories with ${names[random.nextInt(10)]}",
+//   "${names[random.nextInt(10)]} Tagged you and ${random.nextInt(100)} others in a post",
+//   "${names[random.nextInt(10)]} Sent you a friend request",
+// ];
+
+// List notifications = List.generate(
+//     13,
+//     (index) => {
+//           "name": names[random.nextInt(10)],
+//           "dp": "assets/images/cm${random.nextInt(10)}.jpeg",
+//           "time": "${random.nextInt(50)} min ago",
+//           // "notif": notifs[random.nextInt(10)]
+//         });
 
 List posts = List.generate(
     13,
     (index) => {
           "name": names[random.nextInt(10)],
-          "dp": "assets/images/cm${random.nextInt(10)}.jpeg",
+          "dp": "assets/images/cm${random.nextInt(10)}.jpeg", //profile pic
           "time": "${random.nextInt(50)} min ago",
-          "img": "assets/images/cm${random.nextInt(10)}.jpeg"
+          "img": "assets/images/em${random.nextInt(10)}.jpeg",
+          "foodname": foodnames[random.nextInt(10)],
         });
 
 List types = ["text", "image"];
