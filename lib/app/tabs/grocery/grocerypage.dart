d39e8a7217896f55369a8d6ee66ec8f8bafc8110
punymook <punymook@gmail.com>
2021-06-05 03:11:05 +0700

    Create grocerypage.dart

---
+++ lib/app/tabs/grocery/grocerypage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:foodgook/app/models/newbook_model.dart';
+import 'package:foodgook/app/models/popularbook_model.dart';
+import 'package:foodgook/app/components/custom_tab_indicator.dart';
+
+class GroceryPage extends StatefulWidget {
+  @override
+  _GroceryPageState createState() => _GroceryPageState();
+}
+
+class _GroceryPageState extends State<GroceryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(left: 25, top: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Grocery',
+                      style: TextStyle(
+                        // fontFamily: GoogleFonts.openSans ,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.black,
+                        fontFamily: 'Rublik',
+                      ),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
