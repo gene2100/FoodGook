40b8cea09f7cfdcb5d9747f07ee81afade88deef
punymook <punymook@gmail.com>
2021-06-04 20:23:52 +0700

    Fix bottom nav

--- lib/app/tabs/feeds/feeds.dart
+++ lib/app/tabs/feeds/feeds.dart
@@ -17,174 +17,84 @@ class FeedsPage extends StatefulWidget {
 }
 
 class _FeedsPageState extends State<FeedsPage> with TickerProviderStateMixin {
-  var selectedIndex = 0;
-  TabController _tabController;
+  int _selectedIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _tabController = new TabController(length: 5, vsync: this);
-  }
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Recipes',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Forum',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Grocery',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Profile',
+      style: optionStyle,
+    ),
+  ];
 
-  @override
-  void dispose() {
-    super.dispose();
-    _tabController.dispose();
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     // final size = MediaQuery.of(context).size;
     Color _notiIconColor = Colors.grey;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      bottomNavigationBar: Material(
-        color: Colors.white,
-        child: TabBar(
-            controller: _tabController,
-            indicatorColor: Colors.orange[700],
-            labelColor: Color(0xffff6240),
-            unselectedLabelColor: Colors.black54,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.home),
-                text: 'Home',
-              ),
-              Tab(
-                icon: Icon(Icons.receipt),
-                // icon: Icon(Icons.local_restaurant),
-                text: 'Recipes',
-              ),
-              Tab(
-                icon: Icon(Icons.forum),
-                text: 'Forum',
-              ),
-              Tab(
-                icon: Icon(Icons.local_grocery_store),
-                text: 'Grocery',
-              ),
-              Tab(
-                icon: Icon(Icons.person),
-                text: 'Profile',
-              ),
-            ]),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      body: TabBarView(
-        children: <Widget>[
-          Column(
-            //safe area and nested tab
-            children: [
-              Container(
-                child: SafeArea(
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(horizontal: 21.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Row(
-                            //search bar and notifications
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Expanded(
-                                child: Container(
-                                  child: TextField(
-                                    decoration: InputDecoration(
-                                      contentPadding: EdgeInsets.symmetric(
-                                          vertical: 10.0, horizontal: 10.0),
-                                      fillColor: Color(0xfff2f2f2),
-                                      filled: true,
-                                      hintText: 'Search for people, recipes...',
-                                      border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(24.0),
-                                        borderSide: BorderSide.none,
-                                      ),
-                                      prefixIcon: Icon(
-                                        Icons.search,
-                                        size: 24,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              // Row(
-                              //   children: [
-                              //     TextField(
-                              //       decoration: InputDecoration(
-                              //         contentPadding: EdgeInsets.symmetric(
-                              //             vertical: 10.0, horizontal: 10.0),
-                              //         fillColor: Color(0xfff2f2f2),
-                              //         filled: true,
-                              //         hintText: 'Search for people, recipes...',
-                              //         border: OutlineInputBorder(
-                              //           borderRadius:
-                              //               BorderRadius.circular(24.0),
-                              //           borderSide: BorderSide.none,
-                              //         ),
-                              //         prefixIcon: Icon(
-                              //           Icons.search,
-                              //           size: 24,
-                              //         ),
-                              //       ),
-                              //     ),
-                              //   ],
-                              // ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
 
-                              Row(
-                                children: [
-                                  IconButton(
-                                    icon: Icon(Icons.notifications_none),
-                                    color: Colors.grey,
-                                    iconSize: 24,
-                                    tooltip: 'Show all notifications',
-                                    //Still can't change color when clicked
-                                    onPressed: () {
-                                      setState(() {
-                                        if (_notiIconColor == Colors.grey) {
-                                          _notiIconColor = Color(0xffff6240);
-                                        } else {
-                                          _notiIconColor = Colors.grey;
-                                        }
-                                      });
-                                    },
-                                    padding: const EdgeInsets.all(15.0),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              NestedTabBar(),
-            ],
+        iconSize: 25.0, //20
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+            backgroundColor: Colors.red,
           ),
-          Center(
-            // child: Text("Recipes"),
-            child: RecipesView(),
-            // RecipesPage(),
-            //Add page route
+          BottomNavigationBarItem(
+            label: 'Recipes',
+            icon: Icon(Icons.receipt),
+            backgroundColor: Colors.red,
           ),
-          Center(
-            child: Text("Forum"),
+          BottomNavigationBarItem(
+            label: 'Forum',
+            icon: Icon(Icons.forum),
+            backgroundColor: Colors.red,
           ),
-          Center(
-            child: Text("Grocery"),
-            //Add page route
+          BottomNavigationBarItem(
+            label: 'Grocery',
+            icon: Icon(Icons.local_grocery_store),
+            backgroundColor: Colors.red,
           ),
-          Center(
-            // child: Text("Profile"),
-            child: ProfileView(),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person),
+            backgroundColor: Colors.red,
           ),
         ],
-        controller: _tabController,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xffff6240),
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
       floatingActionButton: SpeedDial(
         animatedIcon: AnimatedIcons.menu_close,
